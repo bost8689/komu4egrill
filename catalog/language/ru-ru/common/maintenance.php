e5eec07bbd8509778e8a8141ed2e78446101ff07
Bogdanov Stanislav <bost8689@yandex.ru>
2020-09-01 19:25:09 +0500

    save

--- catalog/language/ru-ru/common/maintenance.php
+++ catalog/language/ru-ru/common/maintenance.php
@@ -4,5 +4,5 @@ $_['heading_title']    = 'Режим обслуживания';
 
 // Text
 $_['text_maintenance'] = 'Магазин временно закрыт';
-$_['text_message']     = '<h1 style="text-align:center;">Магазин временно закрыт. Мы выполняем профилактические работы.<br />Вскоре магазин будет доступен. Пожалуйста, зайдите позже.</h1>';
+$_['text_message']     = '<h2 style="text-align:center;">КомуЧё GRILL сегодня проводит санитарный день.<br />Вскоре Интернет-магазин будет доступен. Пожалуйста, заходите завтра.<br /> следите за новостями на наших страницах: <br/><br/> <a href="https://vk.com/komu4egrill">ВКонтакте</a> <br/><br/><a href="https://www.instagram.com/komu4egrill">Instagram</a> </h1>';
 
