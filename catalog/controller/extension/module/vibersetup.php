57009b5655d32fb2e4eb9b2c3104dc7f40494e32
Stanislav <bost8689>
2021-04-03 16:52:49 +0500

    save

--- catalog/controller/extension/module/vibersetup.php
+++ catalog/controller/extension/module/vibersetup.php
@@ -1,20 +1,20 @@
 <?php
 //require_once("../vendor/autoload.php");
-require_once(DIR_MAIN . 'vendor/autoload.php');
-use Viber\Client;
+// require_once(DIR_MAIN . 'vendor/autoload.php');
+// use Viber\Client;
 class ControllerExtensionModuleVibersetup extends Controller {
 	public function index() {
 		
 		$apiKey = VIBER_TOKEN; // <- PLACE-YOU-API-KEY-HERE
-
-		$webhookUrl = 'https://komu4egrill.ru/index.php?route=extension/module/viberbot'; // <- PLACE-YOU-HTTPS-URL
-		try {
-		    $client = new Client([ 'token' => $apiKey ]);
-		    $result = $client->setWebhook($webhookUrl);
-		    echo "Success!\n";
-		} catch (Exception $e) {
-		    echo "Error: ". $e->getError() ."\n";
-		}
+		echo "string";
+		// $webhookUrl = 'https://komu4egrill.ru/index.php?route=extension/module/viberbot'; // <- PLACE-YOU-HTTPS-URL
+		// try {
+		//     $client = new Client([ 'token' => $apiKey ]);
+		//     $result = $client->setWebhook($webhookUrl);
+		//     echo "Success!\n";
+		// } catch (Exception $e) {
+		//     echo "Error: ". $e->getError() ."\n";
+		// }
 
 		}
 }//class
